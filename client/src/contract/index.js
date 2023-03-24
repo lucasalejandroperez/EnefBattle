b1395a70366901e6834391c6a8e48e0b656f2e7f
Lucas Perez <lucas.alejandro.perez@gmail.com>
2023-03-24 17:53:34 -0300

    Commit whole project

---
+++ client/src/contract/index.js
@@ -0,0 +1,5 @@
+import contract from './EnefBattle.json';
+
+// export const ADDRESS = '0xC6825E381F728a0300f3FD1bf82d9B378FFD83eA';
+export const ADDRESS = '0x2a53C9A4110E52A2569b3EAe30aa2A2d173583e5';
+export const { abi: ABI } = contract;
